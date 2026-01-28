baead5a54816de887cba0e04c13e40faee8e58a9
Álvaro Serrano <serranoalvaroluna@gmail.com>
2026-01-29 00:36:07 +0100

    dos ejemplos mas del uso del if

--- ideaspropias_miniproyectos/pruebas_if.py
+++ ideaspropias_miniproyectos/pruebas_if.py
@@ -32,3 +32,34 @@ elif edad < MAYORIA_EDAD:
     print(f'Tines {edad} años eres menor de edad, no puedes conducir.')
 else:
     print('Datos no validos.')
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# Prueba 3 - Crear un programa que pida al usuario un número y determine si es par o impar.
+
+print('*** Par o Impar ***')
+numero_usuario1 = int(input('\nIntroduce el numero: '))
+numero_par = numero_usuario1 % 2 == 0
+
+if numero_par:
+    print(f'El número {numero_usuario1} es par.')
+else:
+    print(f'El número {numero_usuario1} no es par.')
+
+
+# --------------------------------------------------------------------------------------------------------------------
+# Prueba 4 - Crear un programa que pida al usuario su calificación en un examen y determine si ha aprobado o no.
+
+print('*** Aprobado o No Aprobado ***')
+calificacion_examen = float(input('Introduce tu nota del examen (Del 0 al 10): '))
+
+if calificacion_examen >= 5 and calificacion_examen < 7:
+    print(f'Enhorabuena, con un {calificacion_examen:.2f} tienes un Suficiente')
+elif calificacion_examen >= 7 and calificacion_examen < 9:
+    print(f'Enhorabuena, con un {calificacion_examen:.2f} tienes un Notable')
+elif calificacion_examen >= 9 and calificacion_examen <= 10:
+    print(f'Enhorabuena, con un {calificacion_examen:.2f} tienes un Sobresaliente')
+elif calificacion_examen < 5:
+    print(f'Mas suerte la próxima, con un {calificacion_examen:.2f} tienes un Suspenso')
+else:
+    print('Introduce un valor correcto.')
